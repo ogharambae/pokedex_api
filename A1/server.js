77bdb4c21fa1ca8c3b5a8776e79f4059a86441a3
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-10-18 11:33:22 -0700

    midterm submission

--- A1/server.js
+++ A1/server.js
@@ -171,60 +171,60 @@ app.delete('/api/v1/pokemon/:id', (req, res) => {
         })
 })
 
-function paramHandler(req, res, next) {
-    let check = req.query;
+// function paramHandler(req, res, next) {
+//     let check = req.query;
 
-    const validParams = [
-        "base",
-        "name",
-        "id",
-        "type",
-        "filteredProperty",
-        "name.english",
-        "name.chinese",
-        "name.japanese",
-        "name.french",
-        "base.HP",
-        "base.Attack",
-        "base.Speed Attack",
-        "base.Speed Defense",
-        "base.Speed",
-        "page",
-        "hitsPerPage",
-        "sort"
-    ];
-    Object.keys(check)
-        .forEach((k) => {
-            if (!validParams.includes(k)) {
-                res.send({ errMsg: "Error: Invalid parameter. Please double check your parameter names." });
-                return;
-            }
-        })
-    next();
-}
+//     const validParams = [
+//         "base",
+//         "name",
+//         "id",
+//         "type",
+//         "filteredProperty",
+//         "name.english",
+//         "name.chinese",
+//         "name.japanese",
+//         "name.french",
+//         "base.HP",
+//         "base.Attack",
+//         "base.Speed Attack",
+//         "base.Speed Defense",
+//         "base.Speed",
+//         "page",
+//         "hitsPerPage",
+//         "sort"
+//     ];
+//     Object.keys(check)
+//         .forEach((k) => {
+//             if (!validParams.includes(k)) {
+//                 res.send({ errMsg: "Error: Invalid parameter. Please double check your parameter names." });
+//                 return;
+//             }
+//         })
+//     next();
+// }
 
-function pageHandler(req, res, next) {
-    let check = req.query.page;
-    let checkTwo = req.query.hitsPerPage;
-    if (check && isNaN(check)) {
-        res.send({ errMsg: "Error: page must be a number." })
-        return;
-    } else if (checkTwo && isNaN(checkTwo)) {
-        res.send({ errMsg: "Error: hitsPerPage must be a number." })
-        return;
-    }
-    next();
-}
+// function pageHandler(req, res, next) {
+//     let check = req.query.page;
+//     let checkTwo = req.query.hitsPerPage;
+//     if (check && isNaN(check)) {
+//         res.send({ errMsg: "Error: page must be a number." })
+//         return;
+//     } else if (checkTwo && isNaN(checkTwo)) {
+//         res.send({ errMsg: "Error: hitsPerPage must be a number." })
+//         return;
+//     }
+//     next();
+// }
 
-function typeHandler(req, res, next) {
-    let check = req.query.type;
-    if (check && isNaN(check)) {
-        next();
-    } else {
-        res.send({ errMsg: "Error: type must be a string." });
-        return;
-    }
-}
+// function typeHandler(req, res, next) {
+//     let check = req.query.type;
+//     if (check && isNaN(check)) {
+//         next();
+//     } else {
+//         res.send({ errMsg: "Error: type must be a string." });
+//         return;
+//     }
+// }
 
 // function baseHandler(req, res, next) {
 //     let check = req.query.base.HP;
@@ -238,60 +238,122 @@ function typeHandler(req, res, next) {
 //     next();
 // }
 
-var errHandler = [paramHandler, pageHandler, typeHandler];
+// var errHandler = [paramHandler, pageHandler, typeHandler];
 
-app.get('/pokemonsAdvancedFiltering', errHandler, async (req, res) => {
-    let query = req.query;
-    let { type, sort, page, hitsPerPage } = req.query
+// app.get('/pokemonsAdvancedFiltering', async (req, res) => {
+//     let query = req.query;
+//     let { type, sort, page, hitsPerPage } = req.query
 
-    if (type) {
-        const types = query.type.split(',').map(item => item.trim());
-        type = { $in: types };
-    }
+//     if (type) {
+//         const types = query.type.split(',').map(item => item.trim());
+//         type = { $in: types };
+//     }
 
-    let sortBy = {};
+//     let sortBy = {};
 
-    if (sort) {
-        query.sort.split(",")
-            .map((s) => s.trim())
-            .forEach((e) => {
-                if (e[0] === '-') {
-                    sortBy[e.substring(1)] == -1
-                } else {
-                    sortBy[e] = 1
-                }
-            });
-    }
+//     if (sort) {
+//         query.sort.split(",")
+//             .map((s) => s.trim())
+//             .forEach((e) => {
+//                 if (e[0] === '-') {
+//                     sortBy[e.substring(1)] == -1
+//                 } else {
+//                     sortBy[e] = 1
+//                 }
+//             });
+//     }
 
-    let filterArr = {};
+//     let filterArr = {};
 
-    if (query.filteredProperty) {
-        filterArr = query.filteredProperty.split(',').map(item => item.trim());
-    }
+//     if (query.filteredProperty) {
+//         filterArr = query.filteredProperty.split(',').map(item => item.trim());
+//     }
 
-    if (page) {
-        page = parseInt(page);
-    } else {
-        page = 1;
-    }
+//     if (page) {
+//         page = parseInt(page);
+//     } else {
+//         page = 1;
+//     }
 
-    if (hitsPerPage) {
-        hitsPerPage = parseInt(hitsPerPage);
-    } else {
-        hitsPerPage = 5;
-    }
+//     if (hitsPerPage) {
+//         hitsPerPage = parseInt(hitsPerPage);
+//     } else {
+//         hitsPerPage = 5;
+//     }
 
-    const pokemon = await pokemonModel.find(query).sort(sortBy).select(filterArr).limit(page * hitsPerPage);
+//     const pokemon = await pokemonModel.find(query).sort(sortBy).select(filterArr).limit(page * hitsPerPage);
 
-    res.send({
-        hits: pokemon,
-        page: query.page,
-        numHits: pokemon.length,
-        numPages: Math.ceil(pokemon.length / hitsPerPage),
-        hitsPerPage: query.hitsPerPage,
-        params: req.query
-    });
+//     res.send({
+//         hits: pokemon,
+//         page: query.page,
+//         numHits: pokemon.length,
+//         numPages: Math.ceil(pokemon.length / hitsPerPage),
+//         hitsPerPage: query.hitsPerPage,
+//         params: req.query
+//     });
+// })
+
+app.get("/pokemonsAdvancedFiltering", async (req, res) => {
+
+    let query = {};
+    let operatorsBefore = {};
+    let stat = [];
+    let extractedOps = [];
+    let converted = [];
+    let value = [];
+
+    Object.keys(req.query).forEach((q) => {
+        if (q.includes("comparisonOperators")) {
+            operatorsBefore = req.query[q].split(",").map((operatorsBefore) => operatorsBefore.trim());
+            query[q] = { $in: operatorsBefore }
+        }
+    })
+    operatorsBefore.forEach((e) => {
+        extracted = e.split(/(<=|>=|==|!=|>|<)/g);
+        stat.push(extracted[0]);
+        extractedOps.push(extracted[1]);
+        value.push(extracted[2]);
+    })
+    extractedOps.forEach((e) => {
+        if (e == "<=") {
+            converted.push("$lte")
+        } else if (e == ">=") {
+            converted.push("$gte")
+        } else if (e == "==") {
+            converted.push("$eq")
+        } else if (e == "!=") {
+            converted.push("$ne")
+        } else if (e == ">") {
+            converted.push("$gt")
+        } else if (e == "<") {
+            converted.push("$lt")
+        }
+    })
+    console.log(stat);
+    console.log(converted);
+    console.log(value);
+    let result = await pokemonModel.find(
+        {
+            base: {
+                stat: { converted: value }
+            },
+        }
+    )
+    res.send(result);
 })
+
+app.patch("/pokemonsAdvancedUpdate", async (req, res) => {
+    const { id, pushOperator } = req.query;
+
+    var cleaned = pushOperator.replace(/\[/g, '');
+    cleaned = cleaned.replace(/\]/g, '');
+    let types = cleaned.split(',').map(item => item.trim());
+    console.log(types);
+
+    await pokemonModel.findOneAndUpdate({ id: id }, { new: true, upsert: false, runValidators: true }, { $addToSet: { type: { $each: types } } });
+    res.send({ msg: "Updated pokemon." })
+})
+
 
 app.get('*', (req, res) => {
     res.send({ errMsg: "Improper route. Check API docs plz." })
