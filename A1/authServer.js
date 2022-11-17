e28066b24df3585aea05e387cc8ee123d0b31c05
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-16 18:00:26 -0800

    create authServer.js for handling authentication

---
+++ A1/authServer.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const {
+    PokemonBadRequest,
+    PokemonDb
+} = require("./errors")
+const { asyncWrapper } = require("./asyncWrapper")
+const { connectAuth } = require("./connectAuth")
+const dotenv = require("dotenv")
+const userModel = require("./userModel")
+const cookieParser = require("cookie-parser")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const app = express();
+dotenv.config();
+
+const start = async () => {
+    await connectAuth();
+    app.listen(process.env.authServerPORT, (err) => {
+        if (err)
+            throw new PokemonDb(err)
+        else
+            console.log(`Server is running on port: ${process.env.authServerPORT}`);
+    })
+}
+start()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.post('/register', asyncWrapper(async (req, res) => {
+    const { username, password, email } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPW = await bcrypt.hash(password, salt);
+    const userWithHashedPW = { ...req.body, password: hashedPW };
+    const user = await userModel.create(userWithHashedPW);
+    res.send(user);
+}))
+
+// login for a user
+app.post('/login', asyncWrapper(async (req, res) => {
+    const { username, password } = req.body;
+    const user = await userModel.findOne({ username });
+    if (!user) {
+        throw new PokemonBadRequest("User not found.");
+    }
+    const isPWCorrect = await bcrypt.compare(password, user.password);
+    if (!isPWCorrect) {
+        throw new PokemonBadRequest("Password is incorrect.");
+    }
+    // create a cookie
+    // attach token to cookie
+    // assign the token to the user in mongodb
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.cookie("auth_token", token, { maxAge: 2 * 60 * 60 * 1000 });
+    await userModel.findOneAndUpdate({ username }, { token: token });
+    // res.header("auth-token", token);
+    res.send(user);
+}))
+
+const auth = asyncWrapper(async (req, res, next) => {
+    // get out the token from cookie
+    // find the user that has the token from the cookie in the db
+    // if there is a match then allow access, else no access
+    const token = req.cookies.auth_token;
+    const userFound = await userModel.findOne({ token: token });
+    if (!userFound) {
+        throw new PokemonBadRequest("Access denied.");
+    }
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        next();
+    } catch (err) {
+        throw new PokemonBadRequest("Invalid token.")
+    }
+})
