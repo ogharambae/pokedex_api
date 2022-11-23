94a662b426efe6cd9341299fbef8fcffa9d2bd1b
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-22 17:34:00 -0800

    copy lab10 into lab11

---
+++ lab11/src/Components/Pokemon.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Pokemon({ pokemon }) {
+    const generateId = (id) => {
+        if (id < 10) {
+            return `00${id}`
+        }
+        if (id < 100) {
+            return `0${id}`
+        }
+        return id;
+    }
+    console.log(pokemon);
+    return (
+        <div>
+            <img src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${generateId(pokemon.id)}.png`} alt={`place ${pokemon.name.english} here`} />
+        </div>
+    )
+}
+
+export default Pokemon
