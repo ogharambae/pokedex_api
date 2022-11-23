50b4c1af0f21725340c202f303dd77c20d6d0f34
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-22 21:01:04 -0800

    refractor code

--- lab11/src/Components/FilteredPagination.js
+++ lab11/src/Components/FilteredPagination.js
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from "react"
 import Page from "./Page";
 import Pagination from './Pagination';
-import axios from 'axios'
+import axios from "axios";
 
 function FilteredPagination({ types, checkedState }) {
-    const [pokemons, setPokemons] = useState([])
+    const [pokemons, setPokemons] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const howManyPerPage = 10;
 
     useEffect(() => {
         axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
-            .then(res => res.data)
-            .then(data => {
+            .then((d) => (d).data)
+            .then((d) => {
                 console.log(checkedState);
-                data = (data.filter(pokemon => checkedState.every((checked, i) => !checked || pokemon.type.includes(types.current[i]))
-                ))
-                return data
+                d = (d.filter((pokemon) => checkedState.every((checked, i) => !checked || pokemon.type.includes(types.current[i]))
+                ));
+                return d;
             })
-            .then(res => {
-                setPokemons(res)
+            .then((d) => {
+                setPokemons((d));
             })
             .catch(err => console.log(err))
-    }, [checkedState])
+    }, [checkedState]);
 
-    const indexLowerBound = currentPage * howManyPerPage
-    const indexUpperBound = indexLowerBound - howManyPerPage
-    const currentPokemons = pokemons.slice(indexUpperBound, indexLowerBound)
-    const numPages = Math.ceil(pokemons.length) / howManyPerPage;
+    const indexLowerBound = currentPage * howManyPerPage;
+    const indexUpperBound = indexLowerBound - howManyPerPage;
+    const currentPokemons = pokemons.slice(indexUpperBound, indexLowerBound);
+    const numPages = Math.ceil(pokemons.length) / howManyPerPage;;
 
     return (
         <>
