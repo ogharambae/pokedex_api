9a1c3187d853d826226f0fff12b2c99760f4cb11
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-27 15:37:48 -0800

    delete unused import

--- A3/client/src/routes.js
+++ A3/client/src/routes.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import Login from "./pages/Login";
 import App from "./pages/App";
