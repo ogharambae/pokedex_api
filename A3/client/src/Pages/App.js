b0ecf9d5bf5565518207f1ebca66fb47616e78ff
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-26 18:37:31 -0800

    install dependencies for mui and axios

--- A3/client/src/Pages/App.js
+++ A3/client/src/Pages/App.js
@@ -1,10 +1,28 @@
 import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { Grid, Box } from "@mui/material";
+
 
 function App() {
+  const types = useRef([]);
+  const [checkedState, setCheckedState] = useState([]);
+  console.log(checkedState);
+
+  useEffect(() => {
+    async function getTypes() {
+      const result = await axios.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json");
+      types.current = result.data.map(type => type.english);
+      setCheckedState(new Array(result.data.length).fill(false))
+    }
+    getTypes();
+  }, [])
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Grid container spacing={0}>
+      <Box>
+        Hello World
+      </Box>
+    </Grid>
   );
 }
 
