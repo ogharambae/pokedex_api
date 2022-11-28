6a2d0184e461aacf8dbd1286b90843f16283fdc8
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-28 14:58:06 -0800

    implement mui into design

--- A3/client/src/Components/SearchBar.js
+++ A3/client/src/Components/SearchBar.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Box, ThemeProvider, FormGroup, FormControlLabel, Checkbox, Typography } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+export const customTheme = createTheme({
+    typography: {
+        fontFamily: "PixGamer"
+    }
+});
 
 function SearchBar({ types, checkedState, setCheckedState }) {
-
     const onChangeHandle = (type) => {
         const index = types.current.indexOf(type);
         const newCheckedState = checkedState.map((item, i) => i === index ? !item : item);
@@ -9,18 +16,39 @@ function SearchBar({ types, checkedState, setCheckedState }) {
     }
 
     return (
-        <div>
-            {
-                types.current.map(type => {
-                    return (
-                        <span key={type}>
-                            <input type="checkbox" name="pokeTypes" value={type} id={type} onChange={() => { onChangeHandle(type) }} />
-                            <label htmlFor={type}>{type}</label>
-                            <br />
-                        </span>
-                    )
-                })
-            }
+        <div className="Login-component" >
+            <ThemeProvider theme={customTheme}>
+                <Box
+                    alignItems="center"
+                    justifyContent="center">
+                    <Typography
+                        sx={{ mt: 5, mb: 2 }}
+                        align="center"
+                        variant="h4"
+                        fontWeight="bold">
+                        Search by Type!
+                    </Typography>
+                    <FormGroup
+                        aria-label="position"
+                        row
+                        sx={{ mt: 3, mb: 5 }}>
+                        {
+                            types.current.map(type => {
+                                return (
+                                    <FormControlLabel control={<Checkbox />}
+                                        label={<Typography
+                                            fontWeight="bold"
+                                            variant="body1">
+                                            {type}
+                                        </Typography>}
+                                        onClick={() => { onChangeHandle(type) }} />
+
+                                )
+                            })
+                        }
+                    </FormGroup>
+                </Box>
+            </ThemeProvider>
         </div>
     )
 }
