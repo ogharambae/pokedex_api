e9334d63fa019d24d135aaa8e9be3df3e52617e1
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-11-30 13:52:44 -0800

    remove strictMode so data will only render once

--- A3/client/src/index.js
+++ A3/client/src/index.js
@@ -9,9 +9,7 @@ import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CssBaseline>
-      <RouterProvider router={router} />
-    </CssBaseline>
-  </React.StrictMode>
+  <CssBaseline>
+    <RouterProvider router={router} />
+  </CssBaseline>
 );
