1072b924969de7383edfa5e68ccfcf5fdfa55b0b
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-12-01 23:34:11 -0800

    add mock data to test chart

---
+++ A3/client/src/utility/MockData.js
@@ -0,0 +1,32 @@
+export const MockData = [
+    {
+        id: 1,
+        year: 2000,
+        xData: 100,
+        yData: 200
+    },
+    {
+        id: 2,
+        year: 2001,
+        xData: 120,
+        yData: 230
+    },
+    {
+        id: 3,
+        year: 2002,
+        xData: 200,
+        yData: 150
+    },
+    {
+        id: 4,
+        year: 2003,
+        xData: 700,
+        yData: 200
+    },
+    {
+        id: 5,
+        year: 2004,
+        xData: 450,
+        yData: 150
+    }
+];
