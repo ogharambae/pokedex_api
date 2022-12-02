d7c3e348380bf679b0de8e7b2056b2baed210ae2
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-12-01 23:34:48 -0800

    test table and chart components with mock data

--- A3/client/src/Pages/Admin.js
+++ A3/client/src/Pages/Admin.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createTheme } from "@mui/material/styles";
-import { ThemeProvider, Box, Typography } from "@mui/material";
+import { ThemeProvider, Box, Typography, Grid } from "@mui/material";
+import axios from "axios";
 
+import filterData from "../utility/FilterApiData";
 import bgImage from "../assets/images/admin-background.jpg";
 import LogoutButton from "../components/LogoutButton";
+import BarChart from "../components/BarChart";
+import { MockData } from "../utility/MockData"
+import Table from "../components/Table"
 
 export const customTheme = createTheme({
     typography: {
@@ -23,25 +28,117 @@ const Admin = () => {
         left: 0
     };
 
+    const [userApiData, setUserApiData] = useState({});
+    const [userEndpointData, setUserEndpointData] = useState({});
+    const [accessRouteLogs, setAccessRouteLogs] = useState({});
+
+    const [chartData, setChartData] = useState({
+        labels: MockData.map((data) => data.year),
+        datasets: [{
+            label: "xData",
+            data: MockData.map((data) => data.xData),
+            backgroundColor: ["green", "blue"]
+        }]
+    });
+
+    const getApiData = async () => {
+        const API_URL = "http://localhost:8000"
+        await axios.get(`${API_URL}/userApi`, {
+            withCredentials: true
+        }).then((response) => {
+            setUserApiData(response.data.userApiData);
+            setUserEndpointData(response.data.userEndpointData);
+            setAccessRouteLogs(response.data.accessRouteLogs);
+            console.log(response.data.userApiData)
+            console.log(response.data.userEndpointData)
+            console.log(response.data.accessRouteLogs)
+        }).then(() => {
+            filterData({ userApiData, userEndpointData, accessRouteLogs });
+        })
+    }
+
+    useEffect(() => {
+        getApiData();
+    }, [])
+
     return (
         <div className="Login-component" style={loginPageStyle}>
             <ThemeProvider theme={customTheme}>
-                <Box
-                    sx={{ mt: 10 }}
-                    display={"flex"}
-                    flexDirection={"column"}
-                    alignItems="center"
-                    justifyContent="center">
-                    <Typography
-                        variant="h1"
-                        fontFamily={"PixGamer"}
-                        paddingTop={5}
-                        fontWeight="bold">
-                        Admin Dashboard
-                    </Typography>
-                    <LogoutButton />
-                </Box>
+                <Box>
 
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            width: "100%",
+                            height: "auto",
+                            mt: 2
+                        }}>
+                        <Typography
+                            variant="h1"
+                            fontFamily={"PixGamer"}
+                            paddingTop={5}
+                            fontWeight="bold">
+                            Admin Dashboard
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        direction="row"
+                        marginTop={2}
+                    >
+                        <Grid
+                            container
+                            spacing={2}>
+                            <Grid
+                                item
+                                xs={4}>
+                                <BarChart chartData={chartData} />
+                            </Grid>
+                            <Grid
+                                item
+                                xs={4}>
+                                <BarChart chartData={chartData} />
+                            </Grid>
+                            <Grid
+                                item
+                                xs={4}>
+                                <BarChart chartData={chartData} />
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+                    <Box
+                        direction="row"
+                        marginTop={2}>
+                        <Grid
+                            container
+                            spacing={2}>
+                            <Grid
+                                item
+                                xs={6}>
+                                <Table />
+                            </Grid>
+                            <Grid
+                                item
+                                xs={6}>
+                                <Table />
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            width: "100%",
+                            height: "auto",
+                            mt: 2
+                        }}>
+                        <LogoutButton />
+                    </Box>
+
+                </Box>
             </ThemeProvider>
         </div>
     )
