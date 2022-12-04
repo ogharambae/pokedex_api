38a854e9a7706cc52487bbe7ff7b96246868f88e
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-12-03 17:55:58 -0800

    implement Table component

--- A3/client/src/Components/Table.js
+++ A3/client/src/Components/Table.js
@@ -2,47 +2,30 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+    { field: 'id', headerName: 'ID', width: 50, flex: 1 },
+    { field: 'user', headerName: 'User', width: 115, flex: 1 },
+    { field: 'count', headerName: 'count', width: 115, flex: 1 }
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, count: 'Snow', user: 'Jon' },
+    { id: 2, count: 'Lannister', user: 'Cersei' },
+    { id: 3, count: 'Lannister', user: 'Jaime' },
+    { id: 4, count: 'Stark', user: 'Arya', age: 16 },
+    { id: 5, count: 'Targaryen', user: 'Daenerys' },
+    { id: 6, count: 'Melisandre', user: 'User' },
+    { id: 7, count: 'Clifford', user: 'Ferrara' },
+    { id: 8, count: 'Frances', user: 'Rossini' }
 ];
 
 export default function DataTable() {
     return (
-        <div style={{ height: 400, width: '100%' }}>
+        <div style={{ height: 375, width: '100%' }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
-                checkboxSelection
             />
         </div>
     );
